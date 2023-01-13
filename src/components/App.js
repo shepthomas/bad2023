d6f430e9cb5e0890781068c0727cb6ef73de7f98
Matt Neal <findme@mattneal.com.au>
2023-01-13 11:15:10 +1000

    Added sass and started type tokens

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="BAD t-body">
+      <p className="t-body t-emphasis">
+        We’re sending BAD out with a bang with a block party that will swallow
+        up Fish Lane, hook, line and sinker.
+      </p>
+      <p>
+        Presented by Blaklash, the BAD Block Party will kick off with an
+        afternoon of markets from local First Nations makers including Rachael
+        Sarra and Magpie Goose. Then take a behind the scenes tour of the
+        laneway’s impressive collection of public artworks before returning to
+        Town Square to sip cocktails amongst the lush greenery of Town Square.
+      </p>
+      <p>
+        Stick around after dark and watch the lane burst into a riot of circus,
+        dance and live tunes. It’s good to be BAD. Book a BAD Art Walk and Talk
+        Tour here.
+      </p>
+      <p>
+        <span className="t-emphasis">
+          Presented by Blaklash in association with Museum of Brisbane and ARIA.
+        </span>
+        <br />
+        BAD is an initiative of Museum of Brisbane and is supported by the
+        Queensland Government through Arts Queensland. This event is also
+        generously supported by ARIA property.
+      </p>
+    </div>
+  );
+}
+
+export default App;
